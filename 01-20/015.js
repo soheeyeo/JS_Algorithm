0b5c0a5672bcd4081af36d14da64d67485c9ad87
soheeyeo <sohee407@gmail.com>
2022-08-20 22:31:44 +0900

    변수명 변경

--- 01-20/015.js
+++ 01-20/015.js
@@ -6,6 +6,6 @@
 // 입력 : 김다정
 // 출력 : 안녕하세요. 저는 김다정입니다.
 
-const name = prompt('이름을 입력하세요');
+const student = prompt('이름을 입력하세요');
 
-console.log(`안녕하세요. 저는 ${name}입니다.`); 
+console.log(`안녕하세요. 저는 ${student}입니다.`);
