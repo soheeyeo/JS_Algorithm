a7094c56f98684f085e876c463a37c8cc824ca70
soheeyeo <sohee407@gmail.com>
2022-09-30 16:05:41 +0900

    70번 문제 풀이

---
+++ 61-80/070.js
@@ -0,0 +1,43 @@
+// 행렬 2개가 주어졌을 때 곱할 수 있는 행렬인지 확인하고 곱할 수 있다면 그 결과를 출력하고,
+// 곱할 수 없다면 -1을 출력하는 프로그램을 만들어주세요.
+
+// 입력
+// a = [[1, 2],
+// 		[2, 4]]
+
+// b = [[1, 0],
+// 		[0, 3]]
+
+// 출력
+// [[1, 6], [2, 12]]
+
+const a = [[1, 2],
+		[2, 4]];
+
+const b = [[1, 0],
+		[0, 3]];
+
+function solution(a, b) {
+    let result = [];
+    const len = a.length;
+
+    if(a[0].length === b.length) {
+        let result = [];
+        for(let i=0; i<len; i++) {
+            let row = [];
+            for(let j=0; j<len; j++) {
+                let el = 0;
+                for(let k=0; k<len; k++) {
+                    el += a[i][k] * b[k][j];
+                }
+                row.push(el);
+            }
+            result.push(row);
+        } 
+        return result;
+    } else {
+        return -1;
+    }
+}
+
+console.log(solution(a, b));
